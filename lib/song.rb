1620949da7016ca6019c77f4f526e85e1beac977
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-06-26 01:21:09 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,36 @@
+class Song
+attr_accessor :name, :artist, :genre
+@@song_count = 0
+
+def initialize(name)
+  @name = name
+  @@song_count += 1
+end
+
+def self.count
+  @@song_count
+end
+
+def self.artists
+end
+  
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+end
